25237182b9844be081dbd77c4c54b5c6c983a946
Raz <razgavrieli@gmail.com>
2023-04-06 14:06:10 +0300

    finished game

--- Assets/Scripts/Collider.cs
+++ Assets/Scripts/Collider.cs
@@ -8,6 +8,9 @@ using UnityEngine;
 public class Collider : MonoBehaviour {
     private Vector3 slowDownSpeed = new Vector3(0.15f, 0f, 0f);
 
+    [SerializeField] private AudioSource squish;
+
+
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Car" && enabled) {
             if (this.GetComponent<Mover>().velocity[0] < -1 && other.transform.position.x < this.transform.position.x) // moving left and to the right of other car, also not moving too slow
@@ -20,13 +23,10 @@ public class Collider : MonoBehaviour {
             }
         } else if (other.tag == "Player" && enabled) {
             //SceneManager.LoadScene("Lose");
-            Destroy(other.gameObject);
+            //Destroy(other.gameObject);
+            other.transform.position = new Vector3(0.408513784f,-3.9173196793f,-2.18516207f);
+            squish.Play();
             Debug.Log("player died");
-            //other.gameObject.transform.position = new Vector3(-0.432281375f,-0.125747204f,-2.08516216f);
-            
-            
-
-
         } 
     }
 
